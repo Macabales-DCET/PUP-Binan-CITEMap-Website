0ee413f9835d3178a6d07cd91a6e190b9272566e
Macabales, Shawn Aaron <macabales.shawnaaron@gmail.com>
2025-04-26 11:50:50 +0800

    Add files via upload

--- citemap_script.js
+++ citemap_script.js
@@ -17,3 +17,23 @@ function floor(floorNumber) {
 window.onload = () => {
     floor(1);
   };
+
+// Room Buttons Testing
+function chapelButton() {
+    alert("This is the campus' Chapel.")
+}
+function r115Button() {
+    alert("This is room no.115.")
+}
+function r104Button() {
+    alert("This is room no.104. It is currently the office of the ACES PUP Binan Organization.")
+}
+function openButton() {
+    alert("An open area behind the campus. It has a basketball/volleyball court. It is also perfect for hosting crowded activities, like concerts.")
+}
+function canteenButton() {
+    alert("This is the campus' canteen. It sells a variety of food and beverages. It also accepts payment via Gcash.")
+}
+function parkingButton() {
+    alert("This open area at the side of the campus is the parking lot.")
+}
